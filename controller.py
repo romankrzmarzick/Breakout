6084bb9758dd3bac15fbe4823ee01a4aa7ebfa83
Roman Krzmarzick <romankrzmarzick@gmail.com>
2026-04-07 09:17:54 +0200

    Working platform and ball physics.
    Introduced physics test to pass into ball and platform for interactions.

--- controller.py
+++ controller.py
@@ -2,20 +2,21 @@ import pygame
 import sys
 from modules.dynamics import Platform, Ball
 
-WINDOW_SIZE = (600, 800)
+WINDOW_SIZE = (600, 700)
 
 
 class Breakout:
     def __init__(self):
         self.screen = pygame.display.set_mode(WINDOW_SIZE)
         self.clock = pygame.time.Clock()
-        
+    
     def run(self):
         platform = Platform()
         ball = Ball()
+        obstacles = platform.rect
+        
         while True:
-           
-           
+            
             # --- Event Phase ---
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -24,19 +25,21 @@ class Breakout:
                 if event.type == pygame.KEYDOWN:
                     if ball.unactive:
                         if event.key == pygame.K_UP:
-                            ball.position[0] = 3   
-                            ball.position[1] = -3
+                            ball.velocity[0] = 4
+                            ball.velocity[1] = -4
                             ball.unactive = False
 
             # --- Input ---
             keys = pygame.key.get_pressed()
-            platform.position[0] = 0
+            platform.velocity[0] = 0
 
             if keys[pygame.K_LEFT]:
-                platform.position[0] = -5
+                platform.velocity[0] = -5
             if keys[pygame.K_RIGHT]:
-                platform.position[0] = 5
-               
+                platform.velocity[0] = 5
+
+            if ball.unactive:
+                ball.velocity[0] = platform.velocity[0]
 
             # Background Color
             self.screen.fill((0, 0, 50))
@@ -47,15 +50,16 @@ class Breakout:
 
             # --- Update --- 
             platform.update()
-            ball.update()
+            ball.update(obstacles)
 
-            if platform.rect.colliderect(ball.rect):
-                ball.position[1] = ball.position[1] * -1
-
+            # --- Interactions ---
+            
+                
+               
             pygame.display.flip()
             self.clock.tick(60)
-
-
+    
+    
 
 
 Breakout().run()
--- modules/dynamics.py
+++ modules/dynamics.py
@@ -2,27 +2,34 @@ import pygame
 
 class Entities:
     def __init__(self, x, y, width, height, color):
-        self.upper_boundary = (600, 800)
-        self.position = [0, 0]
+        self.upper_boundary = (600, 700)
+        self.velocity = [0, 0]
         self.rect = pygame.Rect(x, y, width, height)
         self.color = color
        
 
     def move(self):
-        self.rect.x += self.position[0]
-        self.rect.y += self.position[1]
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
 
     def render(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
     
-    def update(self):
+    def test_collisions(self, *args, **kwargs):
+        collisions = []
+        for obstacle in args:    
+            if self.rect.colliderect(obstacle):
+                collisions.append(obstacle)
+        return collisions
+    
+    def update(self, *args, **kwargs):
         pass
 
 
 
 class Platform(Entities):
     def __init__(self):
-        super().__init__(x=240, y=750, width=120, height=16, color=(255, 255, 255))
+        super().__init__(x=240, y=650, width=120, height=16, color=(255, 255, 255))
     def update(self):
         self.move()
         # Boundaries
@@ -33,24 +40,58 @@ class Platform(Entities):
         
 class Ball(Entities):
     def __init__(self):
-        super().__init__(x=292, y=732, width=16, height=16, color=(255, 0, 0))
+        super().__init__(x=292, y=632, width=16, height=16, color=(255, 0, 0))
         self.radius = 10        
         self.unactive = True
     def render(self, screen):
         pygame.draw.circle(screen, self.color, self.rect.center, self.radius)
-
-    def update(self):
-        self.move()
-        # Y-axis movement
-        if self.rect.top < 0:
-            self.position[1] = self.position[1] * -1
-        if self.rect.bottom > self.upper_boundary[1]:
-            self.position[1] = self.position[1] * -1
         
-        # X-axis movement
-        if self.rect.left < 0:
-            self.position[0] = self.position[0] * -1
-        if self.rect.right > self.upper_boundary[0]:
-            self.position[0] = self.position[0] * -1
-
+    def update(self, obstacles):
+        # --- Y-axis movement & collisions ---
+        self.rect.y += self.velocity[1]
+        collisions = self.test_collisions(obstacles)
         
+        # --- Obstacle collisions ---
+        for collision in collisions:
+            # Ball moving down: hitting the top
+            if self.velocity[1] > 0:
+                self.rect.bottom = collision.top
+                self.velocity[1] *= -1
+            # ball moving up: hitting the bottom
+            elif self.velocity[1] < 0:
+                self.rect.top = collision.bottom
+                self.velocity[1] *= -1
+
+        # --- Y-axis boundary ---
+        if self.rect.top <= 0:
+            self.rect.top = 0
+            self.velocity[1] *= -1
+        if self.rect.bottom >= self.upper_boundary[1]:
+            self.rect.bottom = self.upper_boundary[1]
+            self.velocity[1] *= -1
+
+    # --- X-axis movement & collisions ---
+        self.rect.x += self.velocity[0]
+        collisions = self.test_collisions(obstacles)
+
+        # --- Obstacle collisions ---
+        for collision in collisions:
+            # ball moving right: hitting the left
+            if self.velocity[0] > 0:
+                self.rect.right = collision.left
+                self.velocity[0] *= -1
+            # Ball moving left: hitting the right
+            elif self.velocity[0] < 0:
+                self.rect.left = collision.right
+                self.velocity[0] *= -1
+
+        # --- X-axis boundary ---
+        if self.rect.left <= 0:
+            self.rect.left = 0
+            self.velocity[0] = self.velocity[0] * -1
+        if self.rect.right >= self.upper_boundary[0]:
+            self.rect.right = self.upper_boundary[0]
+            self.velocity[0] = self.velocity[0] * -1
+
+
+       
